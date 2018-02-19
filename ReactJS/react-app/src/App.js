61455fc7d5d0fdc89b55440b9739e6441df09b92
NicholasMcRae <nicholas.mcrae86@gmail.com>
2018-02-19 13:07:10 -0500

    Added binding input

--- ReactJS/react-app/src/App.js
+++ ReactJS/react-app/src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }
+
+  update( e ){
+    this.setState({txt : e.target.value})
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="text"
+         onChange={this.update.bind(this)}/>
+         <h1>{this.state.txt}</h1>
+      </div>
+    )
+  }
+}
 
 export default App
--- ReactJS/react-app/src/index.js
+++ ReactJS/react-app/src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 
 
 ReactDOM.render(
-		<App />,
+		<App txt="this is the prop text"/>,
 		document.getElementById('root')
 	);
